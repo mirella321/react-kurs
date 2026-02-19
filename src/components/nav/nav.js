0a2ae0cf6feb96274e4aba6ae55bc50353a2652d
Sebastian279333 <kawsebastian@gmail.com>
2026-02-19 10:17:27 +0100

    zmiana

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,6 +1,15 @@
+import { Link } from 'react-router-dom';
 import './nav.css';
 
 export function Nav() {
+
+    const navLinks = [
+        {
+            path="/"
+            name:"News"
+            icon: "<i class="fa-solid fa-newspaper"></i>"
+        }
+    ];
 const handleInput = (e) => {
     console.log("kliknięto: ", e.target.value);
 };
@@ -11,19 +20,24 @@ const handleInput = (e) => {
             <span><i class="fa-brands fa-bitcoin"></i></span>
             <ul>
                 <li>
-                    
- 
+                <Link to="/dashboard">   
+
 <i class="fa-brands fa-dashcube"></i>
                     Dashboard
+                    </Link> 
                 </li>
                 <li>
+                    <Link to="/news">
                          <i class="fa-solid fa-newspaper"></i>
                          news
+                     </Link>
             
                 </li>
                 <li>
+                    <Link to="/users">
                     <i class="fa-solid fa-person"></i>
-                    Users</li>
+                    Users
+                    </Link></li>
                     <li>
                         <input placeholder="search" onChange={handleInput}></input>
                     </li>
--- src/components/news/news.js
+++ src/components/news/news.js
@@ -32,3 +32,5 @@ export function News({ title, description }) {
     </div>
   );
 }
+
+
